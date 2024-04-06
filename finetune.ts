93cdef228d8f21f5630903d5c504e0aea5eb7b22
Matt Baker <matthew.reiss.baker@gmail.com>
2024-04-05 21:09:12 -0700

    Initial commit

---
+++ finetune.ts
@@ -0,0 +1,95 @@
+import "dotenv/config";
+import tmp from "tmp";
+import path from "path";
+import fs from "fs/promises";
+import { createReadStream } from "fs";
+import OpenAI from "openai";
+import { validationSample, trainingSample } from ".";
+tmp.setGracefulCleanup();
+
+export const openai = new OpenAI({});
+
+async function waitForUpload(logType: string, id: string) {
+  console.log(`Checking if ${logType} data uploaded...`);
+  while(true) {
+    const retrieved = await openai.files.retrieve(id);
+    if(retrieved.status === 'processed') break;
+    console.log("Not yet processed...");
+    await new Promise<void>(resolve => {
+      setTimeout(resolve, 1000);
+    });
+  }
+  console.log(`Uploaded ${logType} data`);
+}
+
+async function createFinetune(
+  filename: string,
+  trainingData: string,
+  validationData: string,
+  hyperparameters: { epochs?: number, learningRateMultiplier?: number } = {}
+) {
+  console.log("Creating temp file...");
+  const dir = await new Promise<string>((resolve, reject) => {
+    tmp.dir((err, path) => {
+      if(err) reject(err);
+      else resolve(path);
+    });
+  });
+
+  const basename = `${filename}-train.jsonl`;
+  const filepath = path.join(dir, basename);
+  await fs.writeFile(filepath, trainingData);
+  const valBasename = `${filename}-val.jsonl`;
+  const valFilepath = path.join(dir, valBasename);
+  await fs.writeFile(valFilepath, validationData);
+
+  console.log("Uploading...");
+  const file = await openai.files.create({
+    file: createReadStream(filepath),
+    purpose: "fine-tune",
+  });
+  const valFile = await openai.files.create({
+    file: createReadStream(valFilepath),
+    purpose: "fine-tune",
+  });
+
+  await waitForUpload('training', file.id);
+  await waitForUpload('validation', valFile.id);
+
+  const job = await openai.fineTuning.jobs.create({
+    training_file: file.id,
+    validation_file: valFile.id,
+    model: "gpt-3.5-turbo-0125",
+    hyperparameters: {
+      n_epochs: hyperparameters.epochs,
+      learning_rate_multiplier: hyperparameters.learningRateMultiplier,
+    },
+  });
+  console.log("Finetuning job created:");
+  console.log(`https://platform.openai.com/finetune/${job.id}?filter=all`);
+}
+
+type Message = {
+  role: 'user' | 'assistant',
+  content: string,
+};
+
+function prepareSample(sample: { prompt: string, output: string }): Message[] {
+  return [
+    { role: 'user', content: sample.prompt },
+    { role: 'assistant', content: sample.output },
+  ];
+}
+
+const validation: Message[][] = [];
+const training: Message[][] = [];
+for(let i = 0; i < 3000; i++) {
+  validation.push(prepareSample(validationSample()));
+  training.push(prepareSample(trainingSample()));
+}
+
+createFinetune(
+  "clevergpt",
+  training.map(sample => JSON.stringify({ messages: sample })).join('\n'),
+  validation.map(sample => JSON.stringify({ messages: sample })).join('\n'),
+);
---
+++ index.ts
@@ -0,0 +1,216 @@
+const validationTokens = [ 'A', 'B', 'C',  'D' ] as const;
+type ValidationToken = (typeof validationTokens)[number];
+
+const V_DEL = [
+  'A B',
+  'C D',
+];
+const V_SWAP = [
+  'A D',
+  'C B',
+];
+
+const V_RULES = [
+  ...V_DEL,
+  ...V_SWAP,
+];
+
+function computeValidationStep(program: string) {
+  for(const rule of V_RULES) {
+    if(program.match(rule)) {
+      if(V_DEL.indexOf(rule) >= 0) {
+        return program.replace(rule, '');
+      }
+      return program.replace(rule, rule.split(' ').reverse().join(' '));
+    }
+  }
+  return null;
+}
+
+export function validationSample() {
+  const len = Math.floor(Math.random() * 10);
+  const tokens: Array<ValidationToken> = new Array(len);
+  for(let i = 0; i < len; i++) {
+    const tokenIdx = Math.floor(Math.random() * validationTokens.length);
+    tokens[i] = validationTokens[tokenIdx];
+  }
+
+  let program: string | null = tokens.join(' ');
+  const prompt = `ABCD is a system with 4 tokens: A, B, C, and D.
+
+An ABCD program is a sequence of tokens. Example:
+
+    C A D B C
+
+To compute a program, we must rewrite neighbor tokens, using the rules:
+
+    A B ... becomes ... nothing
+    A D ... becomes ... D A
+    C B ... becomes ... B C
+    C D ... becomes ... nothing
+
+For example, the program shown here can be computed step-by-step as:
+
+    C A D B C =
+    C D A B C =
+    A B C =
+    C
+
+The steps were:
+1. We replaced A D with D A
+2. We replaced C D by nothing.
+3. We replaced A B by nothing.
+The final result was just C.
+
+Now, consider the following program:
+
+${program}
+
+Fully compute it, step by step.`;
+
+  const computation: string[] = [];
+  while(program !== null) {
+    computation.push(program);
+    let next = computeValidationStep(program);
+    if(next === program) break;
+    program = next;
+  }
+
+  return {
+    prompt,
+    output: computation.join("=\n"),
+  };
+}
+
+
+type Program = {
+  type: 'terminal',
+} | {
+  type: 'binding-plus',
+  next: Program,
+} | {
+  type: 'binding-minus',
+  next: Program,
+} | {
+  type: 'lambda',
+  body: Program,
+};
+
+function genBody(minLength: number, initial: Program) {
+  const len = minLength + Math.floor(Math.random() * 3);
+  let curr: Program = initial;
+  for(let x = 0; x < len; x++) {
+    curr = {
+      type: Math.random() > 0.5 ? 'binding-plus' : 'binding-minus',
+      next: curr,
+    };
+  }
+  return curr;
+}
+
+function progToString(prog: Program): string {
+  switch(prog.type) {
+    case 'terminal': return 'X';
+    case 'binding-plus': return `X + ${progToString(prog.next)}`;
+    case 'binding-minus': return `X - ${progToString(prog.next)}`;
+    case 'lambda': return `Fn X dot ${progToString(prog.body)}`;
+  }
+};
+
+function computeTrainingStep(prog: Program, data: number) {
+  if(prog.type !== 'lambda') throw `wat`;
+  const output: string[] = [];
+  let curr: Program | null = prog.body;
+  while(curr !== null && curr.type !== 'lambda') {
+    switch(curr.type) {
+      case 'binding-plus':
+        output.push(`${data} +`);
+        break;
+      case 'binding-minus':
+        output.push(`${data} -`);
+        break;
+      case 'terminal':
+        output.push(`${data}`);
+        break;
+    }
+
+    if(curr.type === 'terminal') curr = null;
+    else curr = curr.next;
+  }
+
+  return {
+    output,
+    remaining: curr,
+  };
+}
+
+export function trainingSample() {
+  const bindingsLen = Math.floor(Math.random() * 10);
+  let program: Program = {
+    type: 'lambda',
+    body: genBody(0, { type: 'terminal' }),
+  };
+  for(let i = 0; i < bindingsLen; i++) {
+    program = {
+      type: 'lambda',
+      body: genBody(1, program),
+    };
+  }
+
+  const appDataLen = 1 + Math.floor(Math.random() * bindingsLen);
+  const appData: number[] = [];
+  for(let i = 0; i < appDataLen; i++) {
+    appData.push(Math.floor(Math.random() * 10));
+  }
+
+  const prompt = `REDUCE is a system with five tokens:
+
+1. Fn
+2. dot
+3. X
+4. +
+5. -
+
+A program in REDUCE is a series of tokens, plus application data consisting of the digits 0-9; for
+example:
+
+Program: Fn X dot X + X + Fn X dot X - Fn X dot X
+Application data: 7 8
+
+Fn X dot creates a new variable binding. Moving left to right, any X token can be replaced by the
+actual value of the variable, until you hit a new variable binding for X. To compute a program, you
+simply iteratively apply the application data as variable bindings. For a step-by-step example:
+
+Fn X dot X + X + Fn X dot X - Fn X dot X =
+7 + 7 + Fn X dot X - Fn X dot X =
+7 + 7 + 8 - Fn X dot X
+
+Now, consider the following program:
+
+${progToString(program)}
+
+Given the application data:
+
+${appData.map(num => num.toString()).join(' ')}
+
+Fully compute it, step by step.
+`;
+
+  let output: string[] = [];
+  let curr: Program | null = program;
+  for(const data of appData) {
+    if(curr === null) throw `null program u fucked up`;
+    const computation = computeTrainingStep(curr, data);
+    output = output.concat(computation.output);
+    curr = computation.remaining;
+  }
+
+  if(curr !== null) {
+    output.push(progToString(curr));
+  }
+
+  return {
+    prompt,
+    output: output.join(' '),
+  };
+}
